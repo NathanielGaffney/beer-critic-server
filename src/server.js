eda00b7faf6b4340e98e23ff7d5086b96874a9af
NathanielGaffney <nate440@gmail.com>
2021-07-30 11:10:38 -0400

    added ssl values

--- src/server.js
+++ src/server.js
@@ -6,7 +6,10 @@ const { PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL,
+  connection: (DATABASE_URL + "?sslmode=require"),
+  ssl: {
+    rejectUnauthorized: false
+  },
 })
 
 app.set('db', db)
